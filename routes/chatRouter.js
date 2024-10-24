3b1bbd5c4cfd2e8ba00d6cf62ed28e9ff36bf63e
wkdwlsdjs <wkdwlsdjs@gmail.com>
2024-10-24 17:47:15 +0900

    change links and querys

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const chatService = require('../lib/service/chatService');
 
 // 채팅방 목록 조회 (현재 사용자가 속한 모든 방)
-router.get('/rooms', (req, res) => {
+router.get('/list', (req, res) => {
+    console.log('/chat/list');
     chatService.getChatRooms(req, res);
 });
 
 // 특정 채팅방(CH_NO)에 참여
 router.get('/room/:roomId', (req, res) => {
-    const roomId = req.params.roomId;
+    const roomId = req.params.rooms.CH_NO;
     chatService.getChatRoom(req, res, roomId);
 });
 
