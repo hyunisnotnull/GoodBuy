2d0d857144546b2423fc52a22ecc337e2fe91a16
hyunisnotnull <bongtacker@naver.com>
2024-11-06 02:02:27 +0900

    add naver login

--- public/js/user/modify_form.js
+++ public/js/user/modify_form.js
@@ -1,35 +1,51 @@
-function modifyForm() {
+const modifyForm = () => {
     console.log('modifyForm()');
 
     let form = document.modify_form;
-    
+
+    // 연락처 형식 검사 (000-0000-0000 형식)
+    const phoneRegex = /^\d{3}-\d{4}-\d{4}$/;
+    if (!phoneRegex.test(form.u_phone.value)) {
+        alert('연락처는 000-0000-0000 형식이어야 합니다.');
+        form.u_phone.focus();
+        return;
+    }
+
+    // 닉네임
     if (form.u_nick.value === '') {
         alert('닉네임을 입력해주세요.');
         form.u_nick.focus();
-
-    } else if (form.u_phone.value === '') {
-        alert('연락처를 입력해주세요.');
-        form.u_phone.focus();
-
-    } else if (form.u_sex.value === '') {
-        alert('성별을 선택해주세요.');
-        form.u_sex.focus();
-
-    } else if (form.u_age.value === '') {
-        alert('연령을 선택해주세요.');
-        form.u_age.focus();
-
-    } else if (form.u_post_address.value === '') {
-        alert('우편 주소를 입력해주세요.');
-        form.u_post_address.focus();
-
-    } else if (form.u_detail_address.value === '') {
-        alert('상세 주소를 입력해주세요.');
-        form.u_detail_address.focus();
-
-    } else {
-        form.submit();
-
+        return;
     }
 
-}
+    // 성별 선택
+    if (form.u_sex.value === '') {
+        alert('성별을 선택해주세요.');
+        form.u_sex.focus();
+        return;
+    }
+
+    // 연령대 선택
+    if (form.u_age.value === '') {
+        alert('연령대를 선택해주세요.');
+        form.u_age.focus();
+        return;
+    }
+
+    // 우편주소
+    if (form.u_post_address.value === '') {
+        alert('우편 주소를 입력해주세요.');
+        form.u_post_address.focus();
+        return;
+    }
+
+    // 상세 주소
+    if (form.u_detail_address.value === '') {
+        alert('상세 주소를 입력해주세요.');
+        form.u_detail_address.focus();
+        return;
+    }
+
+    // 폼 제출
+    form.submit();
+};
--- public/js/user/sign_up_form.js
+++ public/js/user/sign_up_form.js
@@ -2,44 +2,70 @@ const signupForm = () => {
     console.log('signupForm()');
 
     let form = document.sign_up_form;
-    if (form.u_id.value === '') {
-        alert('ID를 입력해주세요.');
+    
+    // 이메일 형식 검사 (ID)
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(form.u_id.value)) {
+        alert('유효한 이메일 형식을 입력해주세요.');
         form.u_id.focus();
-
-    } else if (form.u_pw.value === '') {
-        alert('비밀번호를 입력해주세요.');
-        form.u_pw.focus();
-
-    } else if (form.u_nick.value === '') {
-        alert('닉네임을 입력해주세요.');
-        form.u_nick.focus();
-
-    } else if (form.u_phone.value === '') {
-        alert('연락처를 입력해주세요.');
-        form.u_phone.focus();
-
-    } else if (form.u_sex.value === '') {
-        alert('성별을 선택해주세요.');
-        form.u_sex.focus();
-
-    } else if (form.u_age.value === '') {
-        alert('연령을 선택해주세요.');
-        form.u_age.focus();
-
-    } else if (form.u_post_address.value === '') {
-        alert('우편 주소를 입력해주세요.');
-        form.u_post_address.focus();
-
-    } else if (form.u_detail_address.value === '') {
-        alert('상세 주소를 입력해주세요.');
-        form.u_detail_address.focus();
-
-    } else {
-        form.submit();
-        
+        return;
     }
 
-}
+    // 비밀번호 형식 검사 (특수문자 포함 6자리 이상)
+    const pwRegex = /^(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$/;
+    if (!pwRegex.test(form.u_pw.value)) {
+        alert('비밀번호는 특수문자를 포함하여 6자리 이상이어야 합니다.');
+        form.u_pw.focus();
+        return;
+    }
+
+    // 연락처 형식 검사 (000-0000-0000 형식)
+    const phoneRegex = /^\d{3}-\d{4}-\d{4}$/;
+    if (!phoneRegex.test(form.u_phone.value)) {
+        alert('연락처는 000-0000-0000 형식이어야 합니다.');
+        form.u_phone.focus();
+        return;
+    }
+
+    // 닉네임
+    if (form.u_nick.value === '') {
+        alert('닉네임을 입력해주세요.');
+        form.u_nick.focus();
+        return;
+    }
+
+    // 성별 선택
+    if (form.u_sex.value === '') {
+        alert('성별을 선택해주세요.');
+        form.u_sex.focus();
+        return;
+    }
+
+    // 연령대 선택
+    if (form.u_age.value === '') {
+        alert('연령대를 선택해주세요.');
+        form.u_age.focus();
+        return;
+    }
+
+    // 우편주소
+    if (form.u_post_address.value === '') {
+        alert('우편 주소를 입력해주세요.');
+        form.u_post_address.focus();
+        return;
+    }
+
+    // 상세 주소
+    if (form.u_detail_address.value === '') {
+        alert('상세 주소를 입력해주세요.');
+        form.u_detail_address.focus();
+        return;
+    }
+
+    // 폼 제출
+    form.submit();
+};
+
 
 const signinForm = () => {
     console.log('signinForm()');
